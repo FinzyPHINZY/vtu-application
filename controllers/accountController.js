630373e51a95bbe6c0d8e8f1f476599c518f992c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-01-25 19:49:08 +0100

    minor fixes

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -88,13 +88,11 @@ export const createSubAccount = async (req, res) => {
   } catch (error) {
     console.error(error);
 
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: 'Internal Server Error',
-        error: error.response.data,
-      });
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+      error,
+    });
   }
 };
 
