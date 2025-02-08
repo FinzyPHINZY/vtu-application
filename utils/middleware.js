7e68a988004ad62137d7b028daf5f07d62ed13ee
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-08 07:26:52 +0100

    chore: removed request body from logs

--- utils/middleware.js
+++ utils/middleware.js
@@ -12,7 +12,6 @@ dotenv.config({});
 export const requestLogger = (req, res, next) => {
   console.log('Method: ', req.method);
   console.log('Path: ', req.path);
-  console.log('Body: ', req.body);
   console.log('...');
   next();
 };
