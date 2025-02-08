e04eee465d9de8304c0123a217eb48df3d0d51bd
miracle73 <nwadiaromiraclechukwuma@gmail.com>
2025-02-08 10:38:08 +0100

    Recent changes

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiService = createApi({
   reducerPath: 'apiService',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://bolddata-bills-payment-1.onrender.com/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://bolddatapay.com/api' }),
   endpoints: (builder) => ({
     requestOtp: builder.mutation({
       query: (email) => ({
