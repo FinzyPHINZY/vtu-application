eeff507af72870064c50d31bcc8544117436ec5a
miracle73 <nwadiaromiraclechukwuma@gmail.com>
2025-02-10 11:10:58 +0100

    Recent changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      includeAssets: ['bolddata.svg'],
       strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'sw.ts',
