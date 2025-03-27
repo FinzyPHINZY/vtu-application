7f9d0afab93d69a6525bb668edc8cdaf9b2c5c56
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-03-27 17:15:10 +0100

    feat

--- services/signature.js
+++ services/signature.js
@@ -1,7 +1,23 @@
 import dotenv from 'dotenv';
-
+import crypto from 'node:crypto';
 dotenv.config();
 
 const privateKey = process.env.PALMPAY_PRIVATE_KEY;
 
-console.log(privateKey);
+function generateSignature(params) {
+  const sortedKeys = Object.keys(params).sort();
+
+  const signingString = sortedKeys
+    .map((key) => `${key}=${params[key]}`)
+    .join('&');
+
+  const sign = crypto.createSign('RSA-SHA256');
+  sign.update(signingString);
+  sign.end();
+
+  const signature = sign.sign(privateKey, 'base64');
+
+  return signature;
+}
+
+export default generateSignature;
