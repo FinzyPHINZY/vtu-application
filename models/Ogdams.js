be633cde0c39deeecc8f0fe7a52ad906e3e772cb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-04-02 12:30:23 +0100

    feat: implement data fetching and purchase from OGDAMS API

---
+++ models/Ogdams.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+const ogDamsSchema = new mongoose.Schema({
+  networkId: {
+    type: Number,
+    default: 2,
+  },
+  planId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  validity: {
+    type: String,
+    required: true,
+  },
+  currency: {
+    type: String,
+    default: 'NGN',
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  sellingPrice: {
+    type: Number,
+    required: true,
+    default: function () {
+      return this.amount;
+    },
+  },
+  type: {
+    type: String,
+    required: true,
+    default: 'AWOOF',
+  },
+  source: {
+    type: String,
+    enum: ['ogdams', 'datastation'],
+    default: 'datastation',
+  },
+  isAvailable: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+const ogDams = mongoose.model('ogDams', ogDamsSchema);
+
+export default ogDams;
