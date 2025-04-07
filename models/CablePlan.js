9068f3a902feec0a29e2ad40d454ad7f06a2c184
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-04-07 15:25:16 +0100

    fix: minor update

--- models/CablePlan.js
+++ models/CablePlan.js
@@ -1,38 +1,42 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const cablePlanSchema = new mongoose.Schema(
-  {
-    cablePlanID: {
-      type: Number,
-      required: true,
-      unique: true,
-    },
-    cablename: {
-      type: String,
-      required: true,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    sellingPrice: {
-      type: Number,
-      required: true,
-      default: function () {
-        return this.amount;
-      },
-    },
-    isAvailable: {
-      type: Boolean,
-      required: true,
-      default: true,
-    },
-  },
-  { timestamps: true }
+	{
+		cablePlanID: {
+			type: Number,
+			required: true,
+			unique: true,
+		},
+		cablename: {
+			type: String,
+			required: true,
+		},
+		package: {
+			type: String,
+			required: true,
+		},
+		amount: {
+			type: Number,
+			required: true,
+		},
+		sellingPrice: {
+			type: Number,
+			required: true,
+			default: function () {
+				return this.amount;
+			},
+		},
+		isAvailable: {
+			type: Boolean,
+			required: true,
+			default: true,
+		},
+	},
+	{ timestamps: true },
 );
 
 cablePlanSchema.index({ cablename: 1, isAvailable: 1 }); // Compound index
 
-const CablePlan = mongoose.model('CablePlan', cablePlanSchema);
+const CablePlan = mongoose.model("CablePlan", cablePlanSchema);
 
 export default CablePlan;
