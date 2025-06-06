ab19b15e58e3ee37bc7af444cf916b3d56fed2bb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-06-06 16:53:45 +0100

    fix: update callback URL for bank transfer order in palmpayController

--- controllers/palmpayController.js
+++ controllers/palmpayController.js
@@ -779,7 +779,7 @@ export const createBankTransferOrder = async (req, res, next) => {
       description: `Deposit of ${amount} NGN to ${user.firstName}'s account`,
       userId: user._id.toString(),
       currency: 'NGN',
-      callBackUrl: `${process.env.FRONTEND_URL}/home`,
+      callBackUrl: `https://www.bolddatahub.com/home`,
       productType: 'bank_transfer',
     };
 
